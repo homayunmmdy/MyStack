d66cc362e51fd9a78c77cdbe393a337b02f16554
Homayoun <homayoun763@gmail.com>
2025-07-08 00:18:11 +0330

    add : env

--- 2.Node-Express/myApp.js
+++ 2.Node-Express/myApp.js
@@ -3,12 +3,20 @@ let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
+require("dotenv").config();
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", function (req, res) {
-  res.json({ message: "Hello json" });
+  let message = "Hello json";
+
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+
+  res.json({ message: message });
 });
 
 module.exports = app;
