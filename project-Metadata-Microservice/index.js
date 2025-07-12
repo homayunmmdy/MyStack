126ebd0002727a60c7d52615ae27ec35bab561ff
Homayoun <homayoun763@gmail.com>
2025-07-12 21:41:37 +0330

    submit: last project

---
+++ project-Metadata-Microservice/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+/*-----------------------------------------------------------------------------------------*/
+/*---------------------------------------MY CODE-------------------------------------------*/
+/*-----------------------------------------------------------------------------------------*/
+
+app.post('/api/fileanalyse',(req,res) => {
+  
+});
+
+
+/*=========================================================================================*/
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
