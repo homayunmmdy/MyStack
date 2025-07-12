c8ac3538ffaf4cad9bb105ba884c38e43ea4db90
Homayoun <homayoun763@gmail.com>
2025-07-12 22:23:19 +0330

    fix : project 5 test 4

--- project-Metadata-Microservice/index.js
+++ project-Metadata-Microservice/index.js
@@ -1,28 +1,37 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 /*-----------------------------------------------------------------------------------------*/
 /*---------------------------------------MY CODE-------------------------------------------*/
 /*-----------------------------------------------------------------------------------------*/
 
-app.post('/api/fileanalyse',(req,res) => {
-  
-});
+app.post("/api/fileanalyse", (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
 
+  const response = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
+
+  res.json(response);
+});
 
 /*=========================================================================================*/
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-    console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
