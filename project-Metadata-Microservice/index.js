541ea20a87865e6c48a84ef38e444917f01fe3d0
Homayoun <homayoun763@gmail.com>
2025-07-12 22:54:56 +0330

    pass : project 5

--- project-Metadata-Microservice/index.js
+++ project-Metadata-Microservice/index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var cors = require("cors");
 require("dotenv").config();
-
 var app = express();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -15,23 +16,26 @@ app.get("/", function (req, res) {
 /*---------------------------------------MY CODE-------------------------------------------*/
 /*-----------------------------------------------------------------------------------------*/
 
-app.post("/api/fileanalyse", (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: "No file uploaded" });
+app.post(
+  "/api/fileanalyse",
+  upload.single("upfile"),
+  function (req, res, next) {
+    if (!req.file) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
+    const response = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    };
+
+    res.json(response);
   }
-
-  const response = {
-    name: req.file.originalname,
-    type: req.file.mimetype,
-    size: req.file.size,
-  };
-
-  res.json(response);
-});
+);
 
 /*=========================================================================================*/
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
